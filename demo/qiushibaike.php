74d661213b292655bf6fa176bc79caeed3f8751d
YangZetao <seatle888@gmail.com>
2016-10-15 14:31:24 +0800

    全新的多进程模型设计，增加运行面板

--- demo/qiushibaike.php
+++ demo/qiushibaike.php
@@ -7,6 +7,7 @@ require dirname(__FILE__).'/../core/init.php';
 
 $configs = array(
     'name' => '糗事百科',
+    'log_show' => true,
     'tasknum' => 1,
     //'save_running_state' => true,
     'domains' => array(
@@ -23,7 +24,6 @@ $configs = array(
         "http://www.qiushibaike.com/article/\d+",
     ),
     'collect_fails' => 5,
-    //'log_show' => true,
     'export' => array(
         'type' => 'csv',
         'file' => PATH_DATA.'/qiushibaike.csv',
