441e1681508ac49dca403ff3695cef88d4efd0f3
YangZetao <seatle888@gmail.com>
2016-11-13 23:26:53 +0800

    实现多服务器集群爬虫

--- core/phpspider.php
+++ core/phpspider.php
@@ -19,7 +19,7 @@ class phpspider
      * 版本号
      * @var string
      */
-    const VERSION = '3.0.0';
+    const VERSION = '3.0.1';
 
     /**
      * 爬虫爬取每个网页的时间间隔,0表示不延时，单位：毫秒
@@ -44,7 +44,6 @@ class phpspider
     /**
      * 爬虫爬取网页所使用的浏览器类型：android，ios，pc，mobile
      */
-    const AGENT = "Mozilla/5.0 PHPSpider/".self::VERSION;
     const AGENT_ANDROID = "Mozilla/5.0 (Linux; U; Android 6.0.1;zh_cn; Le X820 Build/FEXCNFN5801507014S) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 Chrome/49.0.0.0 Mobile Safari/537.36 EUI Browser/5.8.015S";
     const AGENT_IOS = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13G34 Safari/601.1";
     const AGENT_PC = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36";
