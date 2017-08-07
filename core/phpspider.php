5d4655d35eb43c39118ad9a286e523d86cd4b6be
yangzetao <yangzetao@yangzetaodeMacBook-Pro.local>
2017-08-07 20:09:04 +0800

    修复get_fields函数

--- core/phpspider.php
+++ core/phpspider.php
@@ -1809,6 +1809,10 @@ class phpspider
                 // field不为空而且存在子配置
                 if (!empty($values) && !empty($conf['children'])) 
                 {
+                    if (!$values) 
+                    {
+                        $values = array($values);
+                    }
                     $child_values = array();
                     // 父项抽取到的html作为子项的提取内容
                     foreach ($values as $child_html) 
